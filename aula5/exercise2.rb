1d44397273916ada33c5521a5be49e414a12abb0
Gabriel Gamboa <gabriel.gamboa@mblabs.com.br>
2022-08-14 19:30:14 -0300

    feat: finalizando exercicio 2

--- aula5/exercise2.rb
+++ aula5/exercise2.rb
@@ -1,3 +1,15 @@
+require 'cpf_cnpj'
+
 def is_cpf_valid?(cpf)
-    
+    CPF.valid?(cpf)
+end
+
+print "Digite um CPF: "
+cpf = gets.chomp.to_i
+
+
+if is_cpf_valid?(cpf)
+    puts "CPF válido #{CPF.new(cpf).formatted}"
+else 
+    puts "CPF inválido"
 end
